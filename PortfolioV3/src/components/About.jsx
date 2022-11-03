be09b3e001e8cd210ac726afdb983deca88d48f0
Emily Reed <Emilyreed89@gmail.com>
2022-11-03 15:34:07 -0400

    spelling error fixes

--- PortfolioV3/src/components/About.jsx
+++ PortfolioV3/src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Emily.
-            <br className="hidden lg:inline-block" />I love creating and coding with my cat.
+            <br className="hidden lg:inline-block" /> I love creating and coding with my cat.
           </h1>
           <p className="mb-8 leading-relaxed">
             Outside of coding I enjoy playing video games, embroidery, and watching a good new series. I've hidden a little easter egg on my portfolio be sure to let me know if you find it! 
