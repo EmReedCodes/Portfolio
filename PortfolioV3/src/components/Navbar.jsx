ec36952a1340a992346e231c18a53f25d91871d1
Emily Reed <Emilyreed89@gmail.com>
2022-11-14 18:40:11 -0500

    fixed z-index on header, higher z than project now

--- PortfolioV3/src/components/Navbar.jsx
+++ PortfolioV3/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { IconContext } from "react-icons"
 
 const Navbar = () => {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
+    <header className="bg-gray-800 md:sticky top-0 z-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <span className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
