619044d7a555d94b900003b92a1e9c53c726ef1e
Emily Reed <Emilyreed89@gmail.com>
2022-11-15 18:43:01 -0500

    fixed alignment on navbar mobile screens

--- PortfolioV3/src/components/Navbar.jsx
+++ PortfolioV3/src/components/Navbar.jsx
@@ -7,18 +7,18 @@ const Navbar = () => {
     <header className="bg-gray-800 md:sticky top-0 z-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <span className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="#about" className="md:ml-3 text-xl">
             EmReedCodes
           </a>
         </span>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" alt="projects" className="mr-5 hover:text-white">
+          <a href="#projects" alt="projects" className="md:mr-5 m-2 hover:text-white">
             Projects
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="md:mr-5 m-2 hover:text-white">
             Skills
           </a>
-          <a href="#contact" className="mr-5 hover:text-white">
+          <a href="#contact" className="md:mr-5 m-2 hover:text-white">
             Contact
           </a>
         </nav>
